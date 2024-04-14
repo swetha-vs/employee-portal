9de37066e8fa77e8c24331379a51c746804c056a
Swetha V S <166892678+swetha-vs@users.noreply.github.com>
2024-04-14 15:28:39 +0530

    employee portal

---
+++ App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import {Route, Routes } from "react-router-dom"
+import { getAuth} from 'firebase/auth';
+import AuthUser from './pages/login';
+import FormUser from './pages/userList';
+import DetailsUser from './pages/userDetails';
+import ProtectedRoutes from './ProtectedRoutes/ProtectedRoutes';
+import AuthProvider from './pages/AuthContext';
+
+
+function App() {
+  const auth = getAuth();
+
+  return (
+    <AuthProvider>
+      <Routes>
+        <Route path='/login' element={<AuthUser />} />
+        {/* <Route path='/' element={<ProtectedRoutes auth={auth} />} > */}
+          <Route path='/' element={<FormUser />} />
+          <Route path='/DetailsUser/:id' element={<DetailsUser />} />
+        {/* </Route> */}
+      </Routes>
+    </AuthProvider>
+
+  );
+}
+
+export default App;
